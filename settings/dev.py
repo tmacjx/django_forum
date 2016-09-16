73f61561656537a5f3f5008414a9d9073ca71aeb
tmacjx <381221780@qq.com>
2016-09-16 15:57:30 +0800

    Initial commit

---
+++ settings/dev.py
@@ -0,0 +1,17 @@
+from base import *
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_forum',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
+
+}
+
+
